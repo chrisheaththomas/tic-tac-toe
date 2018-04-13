43e6b7c0e7b08791e0958d333ec204c57c448827
Chris Thomas <chris.thomas1@sainsburys.co.uk>
2018-04-13 18:32:34 +0100

    recursive is draw solution

--- src/utilities.js
+++ src/utilities.js
@@ -20,61 +20,34 @@ export function calculateWinner(squares) {
 }
 
 export function isDraw(squares, nextPlayer) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
 
-  if (calculateWinner(squares)) {
-    return false;
-  }
-
-
-  if (squares.filter((s) => {
+  let nullFilter = (s) => {
     return s === null
-  }).length === 2) {
-    let isGameDrawn = true;
-    for (let i = 0; i < lines.length; i++) {
-      const line = Array(3).fill(null);
-
-      for (let j = 0; j < 3; j++) {
-        line[j] = squares[lines[i][j]]
-      }
-      if (line.filter((s) => {
-        return s === null
-      }).length === 1 && line.filter(onlyUnique).length === 2) {
-        isGameDrawn = false;
-      }
-    }
-    if(isGameDrawn) return true;
   }
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = Array(3).fill(null);
-
-    for (let j = 0; j < 3; j++) {
-      line[j] = squares[lines[i][j]]
-    }
-
-    //Not draw if moves left on line and only one side has placed on the line
-    if (line.includes(null) && line.filter(onlyUnique).length < 3) {
-      //Except if there is only 1 move left on the board and it is not the
-      // players turn that previously placed on the line
-      return (squares.filter((s) => {
-            return s === null
-          }).length === 1 &&
-          !line.includes(nextPlayer));
+  function isDrawItr(squares, nextPlayer) {
+    if (calculateWinner(squares)) {
+      return false;
+    } else if (squares.filter(nullFilter).length === 0) {
+      return true;
+    } else {
+      return squares.map((s, i) => {
+        return {square: s, index: i}
+      }).filter((s) => {
+        return s.square === null;
+      }).every((s) => {
+        if (!nullFilter(s.square)) {
+          return false;
+        } else {
+          const nextState = squares.slice();
+          nextState[s.index] = nextPlayer;
+          return isDraw(nextState, nextPlayer === 'O' ? 'X' : 'O');
+        }
+      });
     }
   }
-  return true;
+
+  return isDrawItr(squares, nextPlayer);
+
 }
 
-function onlyUnique(value, index, self) {
-  return self.indexOf(value) === index;
-}
